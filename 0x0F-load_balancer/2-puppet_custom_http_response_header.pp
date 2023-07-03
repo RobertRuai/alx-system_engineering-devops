8ad750734fcad010038476cf47d18b088e6e92ce
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-03 23:54:29 +0300

    creating custom response HTTP header with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+#automate the task of creating a custom HTTP header response
+
+exec {'restart_Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  require  => Exec['add_head']
+}
+
+exec {'add_head':
+  provider => shell,
+  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  require  => Exec['install_Nginx']
+}
+
+exec {'install_Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install Nginx',
+  require  => Exec['update']
+}
+
+exec {'update':
+  provider => shell,
+  command => 'sudo apt-get -y update',
+}
