deaf6c5652bd8561e854ff23eebc5e7826e8d8a5
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-04 14:15:13 +0300

    creating custom response HTTP header with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,26 @@
 #automate the task of creating a custom HTTP header response
 
-exec {'restart_Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  require  => Exec['add_head'],
-}
-
-exec {'add_head':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  require     => Exec['install_Nginx'],
-}
-
-exec {'install_Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install Nginx',
-  require  => Exec['update'],
-}
-
 exec {'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
+
