eab4c44c1a5116980154edf20a952e98cbbcec40
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-24 17:51:32 +0300

    exports data in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Use JSONPlaceholder API to get information about employee"""
+import requests
+import sys
+import csv
+
+
+if __name__ == '__main__':
+    userid = sys.argv[1]
+    u_url = ("https://jsonplaceholder.typicode.com/users/{}".format(
+        userid))
+    res = requests.get(u_url)
+    r = res.json()
+    uname = r.get('username')
+
+    todo_url = ("https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        userid))
+    to_res = requests.get(todo_url).json()
+
+    with open('{}.csv'.format(userid), 'w', encoding='UTF8',  newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in to_res:
+            task_status = task.get('completed')
+            title = task.get('title')
+            writer.writerow([userid, uname, task_status, title])
