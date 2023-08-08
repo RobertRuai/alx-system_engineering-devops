17d49fbd562aac25813f003339ae56ddc2437dca
Robert Ruai Mawich <robertruai4@gmail.com>
2023-08-08 18:48:53 +0300

    recursive function queries Reddit API and returns list of titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""prints ttitles o 1st 10 hot posts listed for a given subreddit"""
+"""
+prints ttitles o 1st 10 hot posts listed 
+for given subreddit recursively
+"""
 import requests
 
 
-def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+def recurse(subreddit, hot_list=[], after=None):
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {
         "User-Agent": "Mozilla/5.0"
     }
+    params = {"after": after}
 
-    res = requests.get(url, headers=headers)
+    res = requests.get(url, headers=headers, params=params)
     if res.status_code == '200':
         res_jsn = res.json()
         posts = res_jsn["data"]["children"]
         for post in posts[:10]:
             title = post["data"]["title"]
-            print(title)
-    print(None)
+            host_list.append(title)
+        l_post = posts[-1]["data"]["name"]
+        return recurse(subreddit, hot_list=hot_list, after=after)
+    else:
+        return None
