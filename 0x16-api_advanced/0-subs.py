657c5cd28c1619b1af43296584a16255f484a190
Robert Ruai Mawich <robertruai4@gmail.com>
2023-08-08 14:57:46 +0300

    returns the number of subscribers (not active users, total subscribers) for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""returns the number of subscribers for a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+        subscribers = response_json["data"]["subscribers"]
+        return subscribers
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return None
